b2f9203e33b10b9218cec3f1b53aed7da75b1bf7
Forrest Blanton <fblanton@gmail.com>
2016-09-02 11:14:34 -0700

    Refactor Code Base #11
    Refined createPanel as a new and improved function createCard with simplefied code and helper functions. Modified CSS to support new design.

--- default.js
+++ default.js
@@ -1,7 +1,11 @@
-var imgHTML = '<img alt=\"140x140\" class=\"img-circle\" src=\"data:image\/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI\/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZkODRiYjUzMiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmQ4NGJiNTMyIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjY4NzUiIHk9Ijc0LjM2NDA2MjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=\" data-holder-rendered=\"true\" style=\"width: 100%; max-height: 140px;\">'; // temporary gray image as a placeholder until images are addded to the data
+// temporary gray image as a placeholder until images are addded to the data
+var imagePlaceholder = 'data:image\/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI\/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZkODRiYjUzMiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmQ4NGJiNTMyIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjY4NzUiIHk9Ijc0LjM2NDA2MjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=';
+var thePlaceholder = element('img', null, ['placeholder']);
+thePlaceholder.src = imagePlaceholder;
 
+//begin gloabl variables
 var shelters = JSON.parse(data);
-var filters = []; // a variable that will hold the current search query
+var filters = [];
 var theQuery = document.getElementById('q-input');
 var theSuggestions = document.getElementById('q-suggestions');
 var theSearch = document.getElementById('search');
@@ -243,7 +247,7 @@ function display (where, shelters) {
   for (var i = 0; i < shelters.length; i++) {
     for (j = 0; j < shelters[i].pets.length; j++) {
       if (shouldDisplay(i, j)) {
-        var theRow = createPanel(shelters[i], j);
+        var theRow = createCard(shelters[i], j);
         theResults.appendChild(theRow);
         count++;
       }
@@ -295,13 +299,11 @@ function update(animal, view) {
   clear(theAnimal);
   clear(theDescription);
 
-  var theImage = document.createElement('div');
-  theImage.innerHTML = imgHTML;
-  theImage.classList.add('centered');
-  theAnimal.appendChild(theImage);
+  var theImage = thePlaceholder.cloneNode();
 
   // create elements for the left hand side of the modal dialog
   elements.push(
+    theImage,
     element('h3', pet.name, ['centered']),
     element('p', pet.breed + ' | ' + pet.gender),
     element('p', 'Adoption Fee: $' + pet.fee),
@@ -327,14 +329,14 @@ function update(animal, view) {
   append(theDescription, elements);
 }
 
-function element(tag, contents, styles, id) {
+function element(tag, contents, classes, id) {
   var theElement = document.createElement(tag);
 
   if (contents) { theElement.textContent = contents; }
 
-  if (styles) {
-    for (var i = 0; i< styles.length; i++) {
-      theElement.classList.add(styles[i]);
+  if (classes) {
+    for (var i = 0; i< classes.length; i++) {
+      theElement.classList.add(classes[i]);
     }
   }
 
@@ -347,65 +349,44 @@ function append(element, children) {
   children.forEach(function(child) {
     element.appendChild(child);
   });
+
+  return element;
 }
 
-function createPanel (shelter, pet) {
-  var theRow = document.createElement('div');  // create a div for the resulting item
-  theRow.classList.add('col-md-4', 'entry');
-  var thePanel = document.createElement('div');
-  thePanel.classList.add('panel','panel-default');
-  thePanel.setAttribute('data-action', 'showDetails');
-  thePanel.setAttribute('data-content',
-    JSON.stringify({shelter: shelter.id, pet: shelter.pets[pet].id})
+function addTo(element, attributes) {
+  attributes.forEach(function(attribute) {
+    element.setAttribute(attribute[0], attribute[1]);
+  });
+
+  return element;
+}
+
+function createCard(shelter, animalID) {
+  var empty = null;
+  var elements = [];
+  var pet = shelter.pets[animalID];
+  var theImage = thePlaceholder.cloneNode();
+  var theCard = element('div', empty, ['col-md-4']);
+  var theEntry = element('div', empty, ['entry']);
+  var theRescueLink = element('a', shelter.name);
+
+  addTo(theEntry, [['data-action', 'showDetails'], ['data-content',
+    JSON.stringify({shelter: shelter.id, pet: pet.id})]]);
+  addTo(theRescueLink, [['data-action', 'showShelter'], ['data-content', shelter.id]]);
+
+  elements.push(
+    theImage,  // currently a global var
+    element('h5', pet.name, ['centered']),
+    element('hr'),
+    element('p', pet.breed),
+    element('p', parseInt(pet.age/12) + ' yrs ' + parseInt(pet.age%12) + ' mos | ' + pet.gender),
+    element('p').appendChild(theRescueLink),
+    element('p', shelter.address.city + ' ' + shelter.address.state)
   );
 
+  theCard.appendChild(append(theEntry, elements));
 
-  var thePanelBody = document.createElement('div');
-  thePanelBody.classList.add('panel-body');
-  thePanel.appendChild(thePanelBody);
-
-  var theImageRow = document.createElement('div');
-  theImageRow.classList.add('row', 'centered');
-  theImageRow.innerHTML = imgHTML;
-  thePanelBody.appendChild(theImageRow);
-
-  var thePetName = document.createElement('h5');
-  thePetName.textContent = shelter.pets[pet].name;
-  thePetName.classList.add('centered');
-  thePanelBody.appendChild(thePetName);
-
-  var theHR = document.createElement('hr');
-  thePanelBody.appendChild(theHR);
-
-  var theBreed = document.createElement('p');
-  theBreed.textContent = shelter.pets[pet].breed;
-  thePanelBody.appendChild(theBreed);
-
-  var theAgeGenderSize = document.createElement('p');
-  theAgeGenderSize.textContent =
-  parseInt(shelter.pets[pet].age/12) + ' yrs '
-  + parseInt(shelter.pets[pet].age%12) + ' mos | '
-  + shelter.pets[pet].gender;
-  // + ' | ' + shelters[i].pets[j].weight;
-  thePanelBody.appendChild(theAgeGenderSize);
-
-  var theRescueParagraph = document.createElement('p');
-  var theRescueName = document.createElement('a');
-  theRescueName.setAttribute('data-action', 'showShelter');
-  theRescueName.setAttribute('data-content', shelter.id);
-  theRescueName.textContent = shelter.name;
-  theRescueParagraph.appendChild(theRescueName);
-  thePanelBody.appendChild(theRescueParagraph);
-
-  var theCityState = document.createElement('p');
-  theCityState.textContent =
-  shelter.address.city + ' '
-  + shelter.address.state;
-
-  thePanelBody.appendChild(theCityState);
-  theRow.appendChild(thePanel);
-
-  return theRow;
+  return theCard;
 }
 
 display('results', shelters);
